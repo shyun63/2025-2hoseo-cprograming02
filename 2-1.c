6c1e8b6a115e4003340b506d36a0de4f458f81c0
shyun63 <zkaqltm@naver.com>
2025-11-11 15:16:16 +0900

    Fix formatting and input handling in 2-1.c

--- 2-1.c
+++ 2-1.c
@@ -10,20 +10,20 @@ int main(){
     int stock[MAX];   //재고 수량
     int id;
     
-    scanf("%d" , &a);      //상품개수 입력
+    scanf("%d" , &a);      
     
-    for(int i = 0; i < a; i++) {    //in << 입고 수량 입력
+    for(int i = 0; i < a; i++) {   
         scanf("%1d", &in[i]);
     }
     
     
-    for(int i = 0; i < a; i++) { // << out판매수량입력
+    for(int i = 0; i < a; i++) { 
         scanf("%1d", &out[i]);
     }
     
-    scanf("%d" , &id);      // id 입력
+    scanf("%d" , &id);      
     
-    for(int i = 0; i < a; i++){     //재고 조사 입고-판매 = 재고
+    for(int i = 0; i < a; i++){     
         stock[i] = in[i] - out[i];
     }
     
